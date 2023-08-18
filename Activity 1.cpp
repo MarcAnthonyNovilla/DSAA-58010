8d95f6c04405e1eecaf7e71cd756dcea7978b2fc
Novilla, Marc <123445459+MarcAnthonyNovilla@users.noreply.github.com>
2023-08-18 08:44:05 +0800

    Add files via upload

---
+++ Activity 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float len, wid, area;
+    cout<<"Enter Length of Rectangle: ";
+    cin>>len;
+    cout<<"Enter Width of Rectangle: ";
+    cin>>wid;
+    area = len*wid;
+    cout<<"\nArea = "<<area;
+    cout<<endl;
+    return 0;
+}
